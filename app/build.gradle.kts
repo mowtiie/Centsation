ebe6843e5b84beb9c20b126737b595855f695e4c
eipna <serbispace@proton.me>
2025-10-09 09:03:18 +0800

    Updated package versions and gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
 android {
     namespace = "com.eipna.centsation"
-    compileSdk = 35
+    compileSdk = 36
 
     dependenciesInfo {
         includeInApk = false
@@ -21,7 +21,7 @@ android {
     defaultConfig {
         applicationId = "com.eipna.centsation"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 7
         versionName = "1.7"
 
@@ -59,11 +59,11 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.preference:preference:1.2.1")
-    implementation("androidx.activity:activity:1.10.1")
+    implementation("androidx.activity:activity:1.11.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
