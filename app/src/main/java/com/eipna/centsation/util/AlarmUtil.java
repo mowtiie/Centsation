be82ab37403e3d3ee1053e781e1c203625cf517e
mowtiie <mowtiie.dev@gmail.com>
2026-06-19 21:01:33 +0800

    Migrated to non-exact alarms

--- app/src/main/java/com/eipna/centsation/util/AlarmUtil.java
+++ app/src/main/java/com/eipna/centsation/util/AlarmUtil.java
@@ -4,7 +4,6 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 
 import com.eipna.centsation.data.Database;
 import com.eipna.centsation.data.saving.Saving;
@@ -12,35 +11,37 @@ import com.eipna.centsation.receiver.DeadlineReceiver;
 
 public class AlarmUtil {
 
-    public static int NO_ALARM = 0;
+    public static final int NO_ALARM = 0;
 
     public static void set(Context context, Saving saving) {
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        if (alarmManager == null) return;
 
         Intent intent = new Intent(context, DeadlineReceiver.class);
         intent.putExtra(Database.COLUMN_SAVING_ID, saving.getID());
         intent.putExtra(Database.COLUMN_SAVING_NAME, saving.getName());
         intent.putExtra(Database.COLUMN_SAVING_DEADLINE, saving.getDeadline());
 
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, saving.getID().hashCode(), intent, PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
-        if (alarmManager != null) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                if (alarmManager.canScheduleExactAlarms()) {
-                    alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, saving.getDeadline(), pendingIntent);
-                }
-            } else {
-                alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, saving.getDeadline(), pendingIntent);
-            }
-        }
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                context,
+                saving.getID().hashCode(),
+                intent,
+                PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
+
+        alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, saving.getDeadline(), pendingIntent);
     }
 
     public static void cancel(Context context, Saving saving) {
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        if (alarmManager == null) return;
 
         Intent intent = new Intent(context, DeadlineReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, saving.getID().hashCode(), intent, PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
-        if (alarmManager != null) {
-            alarmManager.cancel(pendingIntent);
-        }
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                context,
+                saving.getID().hashCode(),
+                intent,
+                PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
+
+        alarmManager.cancel(pendingIntent);
     }
 }
