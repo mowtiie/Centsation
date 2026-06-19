2d0f81b4bb7e48badd4f5a8df2afb09f90ad54de
mowtiie <mowtiie.dev@gmail.com>
2026-06-20 03:11:45 +0800

    Created application class

---
+++ app/src/main/java/com/mowtiie/centsation/CentsationApp.java
@@ -0,0 +1,11 @@
+package com.mowtiie.centsation;
+
+import android.app.Application;
+
+public class CentsationApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
