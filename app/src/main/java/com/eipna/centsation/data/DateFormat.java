7024efc14b35d72d841045f8eb6ffe5d0a8a5116
mowtiie <mowtiie.dev@gmail.com>
2026-06-19 14:09:19 +0800

    Refactored DateFormat enum class

--- app/src/main/java/com/eipna/centsation/data/DateFormat.java
+++ app/src/main/java/com/eipna/centsation/data/DateFormat.java
@@ -1,48 +1,15 @@
 package com.eipna.centsation.data;
 
 public enum DateFormat {
-    EEEE_MMMM_DD_YYYY("Tuesday, April 8 2025", "EEEE, MMMM dd yyyy"),
-    MM_DD_YYYY("04/08/2025", "MM/dd/yyyy"),
-    DD_MM_YYYY("08/04/2025", "dd/MM/yyyy"),
-    YYYY_DD_MM("2025/08/04", "yyyy/dd/MM"),
-    YYYY_MM_DD("2025/04/08", "yyyy/MM/dd");
+    EEEE_MMMM_DD_YYYY("EEEE, MMMM dd yyyy"),
+    MM_DD_YYYY("MM/dd/yyyy"),
+    DD_MM_YYYY("dd/MM/yyyy"),
+    YYYY_DD_MM("yyyy/dd/MM"),
+    YYYY_MM_DD("yyyy/MM/dd");
 
-    private static final DateFormat[] dateFormats;
-
-    static {
-        dateFormats = values();
-    }
-
-    public final String NAME;
     public final String PATTERN;
 
-    DateFormat(String NAME, String PATTERN) {
-        this.NAME = NAME;
-        this.PATTERN = PATTERN;
-    }
-
-    public static String getNameByPattern(String pattern) {
-        for (DateFormat dateFormat : dateFormats) {
-            if (dateFormat.PATTERN.equals(pattern)) {
-                return dateFormat.NAME;
-            }
-        }
-        return DateFormat.MM_DD_YYYY.NAME;
-    }
-
-    public static String[] getNames() {
-        String[] strings = new String[values().length];
-        for (int i = 0; i < strings.length; i++) {
-            strings[i] = dateFormats[i].NAME;
-        }
-        return strings;
-    }
-
-    public static String[] getPatterns() {
-        String[] strings = new String[values().length];
-        for (int i = 0; i < strings.length; i++) {
-            strings[i] = dateFormats[i].PATTERN;
-        }
-        return strings;
+    DateFormat(String pattern) {
+        this.PATTERN = pattern;
     }
 }
