28204859c208d8fa09041eb6ea97a609aaf70f3c
mowtiie <mowtiie.dev@gmail.com>
2026-06-20 03:13:56 +0800

    Created crash handler class

--- app/src/main/java/com/mowtiie/centsation/CentsationApp.java
+++ app/src/main/java/com/mowtiie/centsation/CentsationApp.java
@@ -2,10 +2,13 @@ package com.mowtiie.centsation;
 
 import android.app.Application;
 
+import com.mowtiie.centsation.util.CrashHandler;
+
 public class CentsationApp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        CrashHandler.install(this);
     }
 }
