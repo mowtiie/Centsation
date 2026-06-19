cfdd13bbc095e6b0503a3ca7767e6592f305ecaa
mowtiie <mowtiie.dev@gmail.com>
2026-06-19 14:11:47 +0800

    Optimized settings activity

--- app/src/main/java/com/eipna/centsation/ui/activities/SettingsActivity.java
+++ app/src/main/java/com/eipna/centsation/ui/activities/SettingsActivity.java
@@ -24,7 +24,6 @@ import com.eipna.centsation.R;
 import com.eipna.centsation.data.Contrast;
 import com.eipna.centsation.data.Currency;
 import com.eipna.centsation.data.Database;
-import com.eipna.centsation.data.DateFormat;
 import com.eipna.centsation.data.Theme;
 import com.eipna.centsation.data.saving.Saving;
 import com.eipna.centsation.data.saving.SavingRepository;
@@ -32,7 +31,6 @@ import com.eipna.centsation.data.transaction.Transaction;
 import com.eipna.centsation.data.transaction.TransactionRepository;
 import com.eipna.centsation.databinding.ActivitySettingsBinding;
 import com.eipna.centsation.util.AlarmUtil;
-import com.eipna.centsation.util.PreferenceUtil;
 import com.google.android.material.color.DynamicColors;
 
 import org.json.JSONArray;
@@ -81,11 +79,9 @@ public class SettingsActivity extends BaseActivity {
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
 
-        private PreferenceUtil preferences;
         private SavingRepository savingRepository;
         private TransactionRepository transactionRepository;
 
-        private ListPreference listDeadlineFormat;
         private ListPreference listContrast;
         private ListPreference listTheme;
         private ListPreference listCurrency;
@@ -119,7 +115,7 @@ public class SettingsActivity extends BaseActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.preferences_settings, rootKey);
-            setPreferences();
+            findPreferences();
 
             exportSavings.setOnPreferenceClickListener(preference -> {
                 exportData();
@@ -131,71 +127,47 @@ public class SettingsActivity extends BaseActivity {
                 return true;
             });
 
-            listDeadlineFormat.setEntries(DateFormat.getNames());
-            listDeadlineFormat.setEntryValues(DateFormat.getPatterns());
-            listDeadlineFormat.setValue(preferences.getDeadlineFormat());
-            listDeadlineFormat.setSummary(DateFormat.getNameByPattern(preferences.getDeadlineFormat()));
-            listDeadlineFormat.setOnPreferenceChangeListener((preference, newValue) -> {
-                preferences.setDeadlineFormat((String) newValue);
-                listDeadlineFormat.setSummary(DateFormat.getNameByPattern((String) newValue));
-                return true;
-            });
-
-            listContrast.setEntries(Contrast.toNameArray());
-            listContrast.setEntryValues(Contrast.toValueArray());
-            listContrast.setValue(preferences.getContrast());
-            listContrast.setSummary(Contrast.getName(preferences.getContrast()));
-            listContrast.setOnPreferenceChangeListener((preference, newValue) -> {
-                String selectedContrast = (String) newValue;
-                if (selectedContrast.equals(Contrast.LOW.VALUE)) requireActivity().setTheme(R.style.Theme_Centsation);
-                if (selectedContrast.equals(Contrast.MEDIUM.VALUE)) requireActivity().setTheme(R.style.Theme_Centsation_MediumContrast);
-                if (selectedContrast.equals(Contrast.HIGH.VALUE)) requireActivity().setTheme(R.style.Theme_Centsation_HighContrast);
-                requireActivity().recreate();
-
-                preferences.setContrast(selectedContrast);
-                listContrast.setSummary(Contrast.getName(selectedContrast));
-                return true;
-            });
-
             listCurrency.setEntries(Currency.getNames());
             listCurrency.setEntryValues(Currency.getCodes());
-            listCurrency.setValue(preferences.getCurrency());
-            listCurrency.setSummary(Currency.getName(preferences.getCurrency()));
-            listCurrency.setOnPreferenceChangeListener((preference, currency) -> {
-                preferences.setCurrency((String) currency);
-                listCurrency.setSummary(Currency.getName((String) currency));
-                return true;
-            });
-
-            switchDynamicColors.setVisible(DynamicColors.isDynamicColorAvailable());
-            switchDynamicColors.setChecked(preferences.isDynamicColors());
-            switchDynamicColors.setOnPreferenceChangeListener((preference, isChecked) -> {
-                preferences.setDynamicColors((boolean) isChecked);
-                requireActivity().recreate();
-                return true;
-            });
-
-            switchScreenPrivacy.setChecked(preferences.isScreenPrivacyEnabled());
-            switchScreenPrivacy.setOnPreferenceChangeListener((preference, isChecked) -> {
-                preferences.setScreenPrivacy((boolean) isChecked);
-                requireActivity().recreate();
-                return true;
-            });
-
-            listTheme.setEntries(Theme.getValues());
-            listTheme.setEntryValues(Theme.getValues());
-            listTheme.setSummary(preferences.getTheme());
-            listTheme.setValue(preferences.getTheme());
             listTheme.setOnPreferenceChangeListener((preference, newValue) -> {
                 String selectedTheme = (String) newValue;
                 if (selectedTheme.equals(Theme.SYSTEM.VALUE)) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
                 if (selectedTheme.equals(Theme.BATTERY.VALUE)) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
                 if (selectedTheme.equals(Theme.LIGHT.VALUE)) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 if (selectedTheme.equals(Theme.DARK.VALUE)) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                preferences.setTheme(selectedTheme);
-                listTheme.setSummary(selectedTheme);
                 return true;
             });
+
+            listContrast.setOnPreferenceChangeListener((preference, newValue) -> {
+                requireActivity().recreate();
+                return true;
+            });
+
+            switchDynamicColors.setVisible(DynamicColors.isDynamicColorAvailable());
+            switchDynamicColors.setOnPreferenceChangeListener((preference, newValue) -> {
+                requireActivity().recreate();
+                return true;
+            });
+
+            switchScreenPrivacy.setOnPreferenceChangeListener((preference, newValue) -> {
+                requireActivity().recreate();
+                return true;
+            });
+        }
+
+        private void findPreferences() {
+            savingRepository = new SavingRepository(requireContext());
+            transactionRepository = new TransactionRepository(requireContext());
+
+            listCurrency = findPreference("currency");
+            listTheme = findPreference("theme");
+            listContrast = findPreference("contrast");
+
+            switchDynamicColors = findPreference("dynamic_colors");
+            switchScreenPrivacy = findPreference("screen_privacy");
+
+            exportSavings = findPreference("export");
+            importSavings = findPreference("import");
         }
 
         private boolean noSavingsFound() {
@@ -225,23 +197,6 @@ public class SettingsActivity extends BaseActivity {
             importDataLauncher.launch(importIntent);
         }
 
-        private void setPreferences() {
-            preferences = new PreferenceUtil(requireContext());
-            savingRepository = new SavingRepository(requireContext());
-            transactionRepository = new TransactionRepository(requireContext());
-
-            listDeadlineFormat = findPreference("deadline_format");
-            listCurrency = findPreference("currency");
-            listTheme = findPreference("theme");
-            listContrast = findPreference("contrast");
-
-            switchDynamicColors = findPreference("dynamic_colors");
-            switchScreenPrivacy = findPreference("screen_privacy");
-
-            exportSavings = findPreference("export");
-            importSavings = findPreference("import");
-        }
-
         private void exportJSON(Uri uri) {
             ArrayList<Saving> savings = new ArrayList<>(savingRepository.getAllSavings());
             ArrayList<Transaction> transactions = new ArrayList<>(transactionRepository.getAll());
@@ -256,7 +211,8 @@ public class SettingsActivity extends BaseActivity {
                     savingObject.put(Database.COLUMN_SAVING_NAME, saving.getName());
                     savingObject.put(Database.COLUMN_SAVING_CURRENT_SAVING, saving.getCurrentSaving());
                     savingObject.put(Database.COLUMN_SAVING_GOAL, saving.getGoal());
-                    savingObject.put(Database.COLUMN_SAVING_NOTES, saving.getNotes() == null ? "" : saving.getNotes());
+                    savingObject.put(Database.COLUMN_SAVING_NOTES,
+                            saving.getNotes() == null ? "" : saving.getNotes());
                     savingObject.put(Database.COLUMN_SAVING_IS_ARCHIVED, saving.getIsArchived());
                     savingObject.put(Database.COLUMN_SAVING_DEADLINE, saving.getDeadline());
                     savingJsonArray.put(savingObject);
