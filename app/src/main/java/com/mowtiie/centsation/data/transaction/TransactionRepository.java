aa37622b97530b4a03c030a74368ea3fd33660e7
mowtiie <mowtiie.dev@gmail.com>
2026-06-20 22:55:24 +0800

    Show latest transactions at top

--- app/src/main/java/com/mowtiie/centsation/data/transaction/TransactionRepository.java
+++ app/src/main/java/com/mowtiie/centsation/data/transaction/TransactionRepository.java
@@ -30,7 +30,7 @@ public class TransactionRepository extends Database {
     public ArrayList<Transaction> get(String savingID) {
         ArrayList<Transaction> list = new ArrayList<>();
         SQLiteDatabase database = getReadableDatabase();
-        String query = "SELECT * FROM " + TABLE_TRANSACTION + " WHERE " + COLUMN_TRANSACTION_SAVING_ID + " = ?";
+        String query = "SELECT * FROM " + TABLE_TRANSACTION + " WHERE " + COLUMN_TRANSACTION_SAVING_ID + " = ?" + " ORDER BY " + COLUMN_TRANSACTION_DATE + " DESC";
         Cursor cursor = database.rawQuery(query, new String[]{savingID});
 
         if (cursor.moveToFirst()) {
